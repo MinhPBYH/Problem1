98e38cf6b04df28a5864addb4417877e470915ff
congminh <tacongminh238you@gmail.com>
2022-09-24 11:21:26 +0700

    second commit

--- src/solution/TheLongestWord.java
+++ src/solution/TheLongestWord.java
@@ -21,7 +21,9 @@ public class TheLongestWord {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
+        
+		System.out.print("Input: ");
+		
 		String str = sc.nextLine();
         String longestWord = findLongestWord(str);
         
